87267d08d4a4f34fd7e3e3eb529aeff14b783095
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-08 23:59:52 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Задание 1");
+        int initialAmount = 0;
+        int initialPayment = 15000;
+        int month = 0;
+        while (initialAmount <= 2_459_000) {
+            initialPayment = initialPayment + initialPayment / 100;
+            initialAmount = initialAmount + initialPayment;
+            month++;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + initialAmount + " рублей");
+        }
     }
 }
